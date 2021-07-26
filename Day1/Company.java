d1d15a2c25ff111e205ec3c91d53f069dc6da578
Shubham Phansekar <shubhamphansekar90@gmail.com>
2021-07-26 20:13:43 +0530

    Added Day1 Activities

---
+++ Day1/Company.java
@@ -0,0 +1,38 @@
+
+public class Company {
+
+    private String companyName, employees, teamlead;
+
+    // setter methods
+    public void setName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setEmployees(String employees) {
+        this.employees = employees;
+    }
+
+    public void setTeamlead(String teamlead) {
+        this.teamlead = teamlead;
+    }
+
+    public void setInfo(String companyName, String employees, String teamlead) {
+        setName(companyName);
+        setEmployees(employees);
+        setTeamlead(teamlead);
+    }
+
+    // getter methods
+    String getName() {
+        return companyName;
+    }
+
+    String getEmployees() {
+        return employees;
+    }
+
+    String getTeamlead() {
+        return teamlead;
+    }
+
+}
