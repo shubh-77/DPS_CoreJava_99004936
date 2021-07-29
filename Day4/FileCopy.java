a9de10d2ee86047c9a4896cb95f3b15bc1a1d2fb
shubh-77 <shubhamphansekar90@gmail.com>
2021-07-30 01:26:07 +0530

    Day4 Activity added

---
+++ Day4/FileCopy.java
@@ -0,0 +1,29 @@
+package Day4;
+
+import java.io.*;
+import java.util.*;
+
+public class FileCopy {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Source File");
+        String sourceFile = sc.next();
+        System.out.println("Enter Destination File");
+        String destinationFile = sc.next();
+        FileReader fin = new FileReader(sourceFile);
+        FileWriter fout = new FileWriter(destinationFile, true);
+        int c;
+        while ((c = fin.read()) != -1) {
+            fout.write(c);
+        }
+        System.out.println("Copy finish...");
+        fin.close();
+        fout.close();
+
+        sc.close();
+
+    }
+
+}
